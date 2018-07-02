cd5bed5015f88271e42106877644761b93c2c87a
Lance Govender <lg452323@gmail.com>
2018-07-02 14:00:39 +0200

    Look at this is made a UI

--- app/src/main/java/za/ac/uj/eve/gradhack_mobile/zbraScan.java
+++ app/src/main/java/za/ac/uj/eve/gradhack_mobile/zbraScan.java
@@ -3,11 +3,11 @@ package za.ac.uj.eve.gradhack_mobile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ScanActivity extends AppCompatActivity {
+public class zbraScan extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan);
+        setContentView(R.layout.activity_zbra_scan);
     }
 }
