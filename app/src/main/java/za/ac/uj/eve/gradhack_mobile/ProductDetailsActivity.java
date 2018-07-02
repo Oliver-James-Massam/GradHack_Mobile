6214db7d663de1a15435a5ef2e037fd7a4c5a58b
Oliver-James Massam <ojmassam@gmail.com>
2018-07-02 15:28:25 +0200

    Product Details UI basic start

--- app/src/main/java/za/ac/uj/eve/gradhack_mobile/ProductDetailsActivity.java
+++ app/src/main/java/za/ac/uj/eve/gradhack_mobile/ProductDetailsActivity.java
@@ -2,6 +2,7 @@ package za.ac.uj.eve.gradhack_mobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProductDetailsActivity extends AppCompatActivity {
 
@@ -9,5 +10,18 @@ public class ProductDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+        //Icon in foreground
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_launcher_foreground);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        TextView productName = (TextView) findViewById(R.id.productName);
+        productName.setText("Name: " + getString(R.string.tab)+"Hello");
+
+        TextView productType = (TextView) findViewById(R.id.productType);
+        productType.setText("Type: " + getString(R.string.tab)+"Hello");
+
+        TextView productQuantity = (TextView) findViewById(R.id.productQuantity);
+        productQuantity.setText("Quantity: " + getString(R.string.tab)+"Hello");
     }
 }
